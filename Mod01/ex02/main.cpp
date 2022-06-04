0ca7a29a583d0ddf069155ab158b58ce2a14a0cd
gefaivre <gefaivre@student.42.fr>
2022-06-04 04:32:56 +0200

    mod01 ex02

---
+++ Mod01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string stringREF = string;
+
+    std::cout << "Address of string: " << &string << std::endl;
+    std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
+    std::cout << "Address of stringREF: " << &stringREF << std::endl << std::endl;
+
+    std::cout << "VALUE of string: " << string << std::endl;
+    std::cout << "VALUE of stringPTR: " << stringPTR << std::endl;
+    std::cout << "VALUE of stringREF: " << stringREF << std::endl;
+}
