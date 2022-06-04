64c1dc8f2f6c2841d270dd892eefbbc51527ef88
gefaivre <gefaivre@student.42.fr>
2022-06-04 03:40:20 +0200

    add randomChump function in mod01 ex00

--- Mod01/ex00/main.cpp
+++ Mod01/ex00/main.cpp
@@ -1,20 +1,27 @@
 #include "Zombie.hpp"
-#include "newZombie.cpp"
 #include <iostream>
 #include <string>
 
 
+Zombie* newZombie( std::string name );
+Zombie* randomChump( std::string name );
 
 int main()
 {
-    std::cout << "create with fonction" << std::endl;
-    Zombie *test0 = newZombie( "test0" );
-    test0->announce();
-    delete(test0);
-
     std::cout << "create normal" << std::endl;
-    Zombie test1( "test1" );
-    test1.announce();
+    Zombie test0( "test0" );
+    test0.announce();
+
+    std::cout << "create with fonction" << std::endl;
+    Zombie *test1 = newZombie( "test1" );
+    test1->announce();
+    delete(test1);
+
+    std::cout << "create with randomChump" << std::endl;
+    randomChump( "test3" );
+
+
+
 
 
 }
---
+++ Mod01/ex00/randomChump.cpp
@@ -0,0 +1,10 @@
+#include "Zombie.hpp"
+#include <iostream>
+#include <string>
+
+
+void randomChump( std::string name )
+{
+    Zombie test2( name );
+    test2.announce();
+}
