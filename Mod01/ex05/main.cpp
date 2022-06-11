cd37ecfc1a761a477d74d52b7cf7d7bf0b133f47
gefaivre <gefaivre@student.42.fr>
2022-06-11 14:28:31 +0200

    Mod1 ex05 OK

---
+++ Mod01/ex05/main.cpp
@@ -0,0 +1,16 @@
+#include "Harl.hpp"
+#include <iostream>
+
+
+
+int main(int ac, char *av[])
+{
+	Harl harl;
+	if (ac != 2)
+	{
+		std::cout << "Usage: ./harl [complaint]" << std::endl;
+		return (1);
+	}
+	harl.complain(av[1]);
+	return 0;
+}
