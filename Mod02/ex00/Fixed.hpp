f1522da9562fd322d65804f24acf281d1fa05572
gefaivre <gefaivre@student.42.fr>
2022-06-27 14:44:51 +0200

    ok

--- Mod02/ex00/Fixed.hpp
+++ Mod02/ex00/Fixed.hpp
@@ -1,11 +1,13 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
+
 class Fixed{
 
 public:
 	Fixed( void );
 	~Fixed( void );
 	Fixed(const Fixed &p);
+
 	Fixed&operator=( const Fixed &other );
 	
 	int getRawBits( void ) const ;
