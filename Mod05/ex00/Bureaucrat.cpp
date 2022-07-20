5e4aa1abd6e5028e97e55ba6ceb7e84174003ee2
gefaivre <gefaivre@student.42.fr>
2022-07-20 16:09:39 +0200

    mod05 ex00 OK

---
+++ Mod05/ex00/Bureaucrat.cpp
@@ -0,0 +1,72 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+Bureaucrat::Bureaucrat( void ):name(""), grade(1)
+{
+	std::cout << "Bureaucrat Default Constructor called" << std::endl;
+	checkGrade();
+
+}
+
+Bureaucrat::Bureaucrat( const std::string  name, int grade):name(name), grade(grade)
+{
+	std::cout << "Bureaucrat name and gsrades Constructor called" << std::endl;
+	checkGrade();
+
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &p)
+{
+	std::cout << "Bureaucrat Copy Constructor called" << std::endl;
+	*this = p;
+
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Default Destructor called" << std::endl;
+
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    this->grade = other.grade;
+	return (*this);
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade() const 
+{
+	return (this->grade);
+}
+
+void Bureaucrat::upperGrade(int incrementValue)
+{
+	this->grade -= incrementValue;
+	checkGrade();
+}
+
+void Bureaucrat::lowerGrade(int decrementValue)
+{
+	this->grade += decrementValue;
+	checkGrade();
+}
+
+void Bureaucrat::checkGrade() const
+{
+	if ( this->grade < this->gradeMax )
+		throw Bureaucrat::tooHightExeption();
+	if ( this->grade > this->gradeMin )
+		throw Bureaucrat::tooLowExeption();
+}
+
+std::ostream &	operator<<( std::ostream &os, Bureaucrat const &p)
+{
+	os << p.getName() << ", Bureaucrat grade " << p.getGrade();
+	return os;
+}
+
--- Mod05/ex00/Bureaucrat.hpp
+++ Mod05/ex00/Bureaucrat.hpp
@@ -2,28 +2,58 @@
 #define BUREAUCRAT_HPP
 
 #include <string>
+#include <stdexcept>
 
 
 class Bureaucrat
 {
 public:
-	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const std::string  name, int grade);
 	Bureaucrat(const Bureaucrat &p);
 	~Bureaucrat();
 
 	Bureaucrat &operator=(const Bureaucrat &other);
 
-	std::string getName();
-	int getGrade()
+	std::string getName() const ;
+	int getGrade() const ;
+
+	void upperGrade(int incrementValue);
+	void lowerGrade(int descrementValue);
 
 
 
 private:
 
 	const std::string	name;
-	int			grade;
+	int					grade;
+
+	static const int gradeMax = 1;
+	static const int gradeMin = 150;
+
+	void checkGrade() const;
+
+	Bureaucrat( void );
+
+public:
+
+	class tooHightExeption : public std::exception
+	{
+		const char * what() const throw()
+		{
+			return ("The grade is too Hight");
+		}
+	};
+	class tooLowExeption : public std::exception
+	{
+		const char *  what() const throw()
+		{
+			return ("The grade is too Low");
+		}
+	};
 
 
 };
+
+std::ostream &	operator<<( std::ostream &os, Bureaucrat const &p);
 
 #endif
