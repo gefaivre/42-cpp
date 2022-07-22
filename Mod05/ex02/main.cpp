fbd564bfa584bb469d1eb05a6918234b88702343
gefaivre <gefaivre@student.42.fr>
2022-07-22 11:30:14 +0200

    Mod05 ex02 main+

--- Mod05/ex02/main.cpp
+++ Mod05/ex02/main.cpp
@@ -8,19 +8,24 @@
 
 int main( void )
 {
-    try
-    {
-        Form * test3 = new ShrubberyCreationForm("God");
-        Bureaucrat test("roger", 1);
-        std::cout << *test3 << std::endl;
-        //test.signForm(*test3);
-        test.executeForm(*test3);
-        std::cout << *test3 << std::endl;
+   {
 
-    }
-    catch(std::exception &e)
-    {
-        std::cout << e.what() << std::endl;       
+        Form *test3 = new ShrubberyCreationForm("Garden");
+        try
+        {
+            Bureaucrat test("roger", 1);
+            std::cout << *test3 << std::endl;
+            //test.signForm(*test3);
+            test.executeForm(*test3);
+            std::cout << *test3 << std::endl;
+            delete test3;
+        }
+        catch (std::exception &e)
+        {
+
+            delete test3;
+            std::cout << e.what() << std::endl;
+        }
     }
 
     return (0);
