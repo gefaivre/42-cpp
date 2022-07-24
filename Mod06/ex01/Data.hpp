bf9f9396aa4406c6c912b25473740230c8421c48
gefaivre <gefaivre@student.42.fr>
2022-07-24 19:37:44 +0200

    Mod06 ex01 OK

---
+++ Mod06/ex01/Data.hpp
@@ -0,0 +1,12 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+#include <string>
+
+typedef struct s_Data
+{
+    int number;
+    std::string string;
+}              Data;
+
+
+#endif
--- Mod06/ex01/main.cpp
+++ Mod06/ex01/main.cpp
@@ -1,32 +1,36 @@
 #include "Convert.hpp"
+#include "Data.hpp"
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <iomanip>
 
-int main(int ac, char **av)
+#include <stdint.h>
+
+uintptr_t serialize(Data* ptr);
+
+Data* deserialize(uintptr_t raw);
+
+int main( void )
 {
-    if (ac != 2)
-    {
-        std::cout << "Usage : ./Convert [string]" << std::endl;
-        return (1);
-    }
-    try
-    {
-        Convert conv(av[1]);
-        std::cout << std::setw(7) << "Char "<< ": ";
-        conv.toChar();
-        std::cout << std::setw(7)  << "int " << ": ";
-        conv.toInt();
-        std::cout << std::setw(7)  << "float " << ": ";
-        conv.toFloat();
-        std::cout << std::setw(7)  << "double " << ": ";
-        conv.toDouble();
-    }
-    catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+ 
+	Data		*data= new Data;
+	Data		*new_data;
+	uintptr_t	raw;
+
+    std::cout << "Original :" << std::endl;
+    std::cout << "String : I'm a string" << std::endl;
+    std::cout << "Number : 42" << std::endl << std::endl;
+
+	data->string = "I'm a string";
+	data->number = 42;
+
+
+    std::cout << "**Serialize/Deserialize***" << std::endl << std::endl;
+
+	raw = serialize(data);
+	new_data = deserialize(raw);
+
+    std::cout << "New_data :" << std::endl;
+	std::cout << "String :" << new_data->string << std::endl;
+	std::cout << "Number :" << new_data->number << std::endl;
 
     return (0);
 }
