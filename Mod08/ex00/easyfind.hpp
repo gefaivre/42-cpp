60fb317ece96b07b27044666a5d497f7bcc52d7b
gefaivre <gefaivre@student.42.fr>
2022-07-26 16:52:10 +0200

    Mod08 ex00 0%

---
+++ Mod08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+T easyfind(T *tab, int n)
+{
+
+}
+
+
+#endif
---
+++ Mod08/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <iostream>
+
+int main(void)
+{
+    std::vector<int> test[10];
+
+    return (0);
+
+
+}
